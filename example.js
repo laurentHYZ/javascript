acf4cc7b77422c35ac3e8080b4435a2027500012
laurent <laurent_pinson@hotmail.com>
2025-01-20 14:00:55 +0700

    Create example.js

---
+++ example.js
@@ -0,0 +1,16 @@
+    function showWhenClicked() {
+        const thumbsUp = document.getElementById('appear');
+      const audio = new Audio('./Music/example.mp3'); // Replace with your sound file path
+      audio.play();
+
+      appear.style.display = 'block';
+      appear.style.transform = 'scale(2.5)';
+
+      setTimeout(() => {
+        appear.style.transform = 'scale(2)';
+      }, 500);
+
+      setTimeout(() => {
+        appear.style.display = 'none';
+      }, 2000);
+    }
