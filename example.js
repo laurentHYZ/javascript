f9b6cac3859d201ebd8e766c86054fe282cf7f0f
Laurent P. <lau@hotmail.com>
2025-01-21 09:31:53 +0700

    I am adding a third comment

--- example.js
+++ example.js
@@ -5,7 +5,7 @@
         const thumbsUp = document.getElementById('appear');
       const audio = new Audio('./Music/example.mp3'); // Replace with your sound file path
       audio.play();
-
+      //a third comment////////////////////////////////////////////////////////
       appear.style.display = 'block';
       appear.style.transform = 'scale(2.5)';
       setTimeout(() => {
