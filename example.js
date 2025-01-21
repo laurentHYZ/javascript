a36002cb902877fb248a2ca36801140c78e47e1b
Laurent P. <lau@hotmail.com>
2025-01-21 09:18:45 +0700

    just added a comment

--- example.js
+++ example.js
@@ -1,3 +1,7 @@
+///////////////////////////////////////////////////////////////////////////////
+//                                some comment                               //
+///////////////////////////////////////////////////////////////////////////////
+
     function showWhenClicked() {
         const thumbsUp = document.getElementById('appear');
       const audio = new Audio('./Music/example.mp3'); // Replace with your sound file path
@@ -5,7 +9,6 @@
 
       appear.style.display = 'block';
       appear.style.transform = 'scale(2.5)';
-
       setTimeout(() => {
         appear.style.transform = 'scale(2)';
       }, 500);
